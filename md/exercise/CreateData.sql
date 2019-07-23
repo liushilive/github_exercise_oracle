f1e5c7191aa379bc082be3afdda672fa41b017f1
刘士 <liushilive@outlook.com>
2019-07-23 16:50:10 +0800

    1

--- md/exercise/CreateData.sql
+++ md/exercise/CreateData.sql
@@ -54,82 +54,82 @@ create table SCOTT.SALGRADE
 
 -- 插入数据
 insert into SCOTT.DEPT (DEPTNO, DNAME, LOC)
-values (10, 'ACCOUNTING', 'NEW YORK');
+values (10, '会计', '纽约');
 
 insert into SCOTT.DEPT (DEPTNO, DNAME, LOC)
-values (20, 'RESEARCH', 'DALLAS');
+values (20, '研发部', '达拉斯');
 
 insert into SCOTT.DEPT (DEPTNO, DNAME, LOC)
-values (30, 'SALES', 'CHICAGO');
+values (30, '销售部', '芝加哥');
 
 insert into SCOTT.DEPT (DEPTNO, DNAME, LOC)
-values (40, 'OPERATIONS', 'BOSTON');
+values (40, '运营部', '波士顿');
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (8889, 'wangwu', 'CLERK', 7782, to_date('31-08-2017', 'dd-mm-yyyy'), 1230.00, null, null);
+values (8889, '王五', '普通员工', 7782, to_date('31-08-2017', 'dd-mm-yyyy'), 1230.00, null, null);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7369, 'SM中文', 'CLERK', 7902, to_date('17-12-1980', 'dd-mm-yyyy'), 800.00, null, 20);
+values (7369, 'SM中文', '普通员工', 7902, to_date('17-12-1980', 'dd-mm-yyyy'), 800.00, null, 20);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7499, 'ALLEN', 'SALESMAN', 7698, to_date('20-02-1981', 'dd-mm-yyyy'), 1602.00, 301.00, 30);
+values (7499, 'ALLEN', '销售员', 7698, to_date('20-02-1981', 'dd-mm-yyyy'), 1602.00, 301.00, 30);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7521, 'WARD', 'SALESMAN', 7698, to_date('22-02-1981', 'dd-mm-yyyy'), 1250.00, 500.00, 30);
+values (7521, 'WARD', '销售员', 7698, to_date('22-02-1981', 'dd-mm-yyyy'), 1250.00, 500.00, 30);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7566, 'JONES', 'MANAGER', 7839, to_date('02-04-1981', 'dd-mm-yyyy'), 2975.00, null, 20);
+values (7566, 'JONES', '经理', 7839, to_date('02-04-1981', 'dd-mm-yyyy'), 2975.00, null, 20);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7654, 'MARTIN', 'SALESMAN', 7698, to_date('28-09-1981', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
+values (7654, 'MARTIN', '销售员', 7698, to_date('28-09-1981', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7698, 'BLAKE', 'MANAGER', 7839, to_date('09-06-1981', 'dd-mm-yyyy'), 2850.00, null, 30);
+values (7698, 'BLAKE', '经理', 7839, to_date('09-06-1981', 'dd-mm-yyyy'), 2850.00, null, 30);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7782, 'CLARK', 'MANAGER', 7839, to_date('09-06-1981', 'dd-mm-yyyy'), 2450.00, 1500.00, 10);
+values (7782, 'CLARK', '经理', 7839, to_date('09-06-1981', 'dd-mm-yyyy'), 2450.00, 1500.00, 10);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7788, 'SCOTT', 'ANALYST', 7839, to_date('19-04-1987', 'dd-mm-yyyy'), 3000.00, null, 20);
+values (7788, 'SCOTT', '分析师', 7839, to_date('19-04-1987', 'dd-mm-yyyy'), 3000.00, null, 20);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7789, 'SCOTT', 'ANALYST', 7566, to_date('20-04-1988', 'dd-mm-yyyy'), 5000.00, null, 10);
+values (7789, 'SCOTT', '分析师', 7566, to_date('20-04-1988', 'dd-mm-yyyy'), 5000.00, null, 10);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7839, 'KING', 'PRESIDENT', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 10);
+values (7839, 'KING', '董事长', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 10);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7840, 'KK', 'PRESIDENT', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 20);
+values (7840, 'KK', '董事长', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 20);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7841, 'WK', 'ANALYST', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 30);
+values (7841, 'WK', '分析师', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 30);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7844, 'TURNER', 'SALESMAN', 7698, to_date('08-09-1981', 'dd-mm-yyyy'), 1500.00, 10.00, 30);
+values (7844, 'TURNER', '销售员', 7698, to_date('08-09-1981', 'dd-mm-yyyy'), 1500.00, 10.00, 30);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7876, 'ADAMS', 'CLERK', 7788, to_date('23-05-1987', 'dd-mm-yyyy'), 1100.00, null, 20);
+values (7876, 'ADAMS', '普通员工', 7788, to_date('23-05-1987', 'dd-mm-yyyy'), 1100.00, null, 20);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7900, 'JAMES', 'CLERK', 7698, to_date('03-12-2017', 'dd-mm-yyyy'), 950.00, null, 30);
+values (7900, 'JAMES', '普通员工', 7698, to_date('03-12-2017', 'dd-mm-yyyy'), 950.00, null, 30);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7902, 'FORD', 'ANALYST', 7566, to_date('03-12-2009', 'dd-mm-yyyy'), 3000.00, null, 20);
+values (7902, 'FORD', '分析师', 7566, to_date('03-12-2009', 'dd-mm-yyyy'), 3000.00, null, 20);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7934, 'MILLER', 'CLERK', 7782, to_date('23-01-2000', 'dd-mm-yyyy'), 1300.00, null, 10);
+values (7934, 'MILLER', '普通员工', 7782, to_date('23-01-2000', 'dd-mm-yyyy'), 1300.00, null, 10);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (8888, '_LISI', 'CLERK', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
+values (8888, '_LISI', '普通员工', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (8899, '刘%老师', 'CLERK', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
+values (8899, '刘%老师', '普通员工', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (8877, 'LISI', 'CLERK', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 20);
+values (8877, 'LISI', '普通员工', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 20);
 
 insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (8890, 'Zhang', 'CLERK', 7780, to_date('02-04-2006', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
+values (8890, 'Zhang', '普通员工', 7780, to_date('02-04-2006', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
 
 insert into SCOTT.SALGRADE (GRADE, LOSAL, HISAL)
 values (1, 700, 1200);
