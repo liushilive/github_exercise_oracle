8d87b33ec1aa760be3450ea8bd7c82586ba8ee55
刘士 <liushilive@outlook.com>
2021-10-22 17:47:31 +0800

    1

--- md/exercise/CreateData.sql
+++ md/exercise/CreateData.sql
@@ -1,316 +1,326 @@
 -- 创建用户
 create user scott
-    identified by "scott"
-    default tablespace USERS
-    profile DEFAULT;
+    identified by "scott";
 -- 授予角色权限
-grant connect to SCOTT;
-grant resource to SCOTT;
+grant connect to scott;
+grant resource to scott;
 -- 授予系统权限
-grant create view to SCOTT;
-grant unlimited tablespace to SCOTT;
+grant create view to scott;
+grant unlimited tablespace to scott;
 
 -- 创建表
-create table SCOTT.BONUS
+create table scott.bonus
 (
-    ENAME VARCHAR2(50),
-    JOB   VARCHAR2(20),
-    SAL   NUMBER,
-    COMM  NUMBER
+    ename varchar2(50),
+    job   varchar2(20),
+    sal   number,
+    comm  number
 );
 
-create table SCOTT.DEPT
+create table scott.dept
 (
-    DEPTNO NUMBER(2) not null,
-    DNAME  VARCHAR2(14),
-    LOC    VARCHAR2(13)
-);
-alter table SCOTT.DEPT
-    add constraint PK_DEPT primary key (DEPTNO);
-
-create table SCOTT.EMP
-(
-    EMPNO    NUMBER(4) not null,
-    ENAME    VARCHAR2(50),
-    JOB      VARCHAR2(20),
-    MGR      NUMBER(4),
-    HIREDATE DATE,
-    SAL      NUMBER(7, 2),
-    COMM     NUMBER(7, 2),
-    DEPTNO   NUMBER(2)
-);
-alter table SCOTT.EMP
-    add constraint PK_EMP primary key (EMPNO);
-alter table SCOTT.EMP
-    add constraint FK_DEPTNO foreign key (DEPTNO)
-        references SCOTT.DEPT (DEPTNO);
-
-create table SCOTT.SALGRADE
-(
-    GRADE NUMBER,
-    LOSAL NUMBER,
-    HISAL NUMBER
+    deptno number(2) primary key,
+    dname  varchar2(14),
+    loc    varchar2(13)
 );
 
--- 插入数据
-insert into SCOTT.DEPT (DEPTNO, DNAME, LOC)
-values (10, '会计', '纽约');
-
-insert into SCOTT.DEPT (DEPTNO, DNAME, LOC)
-values (20, '研发部', '达拉斯');
-
-insert into SCOTT.DEPT (DEPTNO, DNAME, LOC)
-values (30, '销售部', '芝加哥');
-
-insert into SCOTT.DEPT (DEPTNO, DNAME, LOC)
-values (40, '运营部', '波士顿');
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (8889, '王五', '普通员工', 7782, to_date('31-08-2017', 'dd-mm-yyyy'), 1230.00, null, null);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7369, 'SM中文', '普通员工', 7902, to_date('17-12-1980', 'dd-mm-yyyy'), 800.00, null, 20);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7499, 'ALLEN', '销售员', 7698, to_date('20-02-1981', 'dd-mm-yyyy'), 1602.00, 301.00, 30);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7521, 'WARD', '销售员', 7698, to_date('22-02-1981', 'dd-mm-yyyy'), 1250.00, 500.00, 30);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7566, 'JONES', '经理', 7839, to_date('02-04-1981', 'dd-mm-yyyy'), 2975.00, null, 20);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7654, 'MARTIN', '销售员', 7698, to_date('28-09-1981', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7698, 'BLAKE', '经理', 7839, to_date('09-06-1981', 'dd-mm-yyyy'), 2850.00, null, 30);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7782, 'CLARK', '经理', 7839, to_date('09-06-1981', 'dd-mm-yyyy'), 2450.00, 1500.00, 10);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7788, 'SCOTT', '分析师', 7839, to_date('19-04-1987', 'dd-mm-yyyy'), 3000.00, null, 20);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7789, 'SCOTT', '分析师', 7566, to_date('20-04-1988', 'dd-mm-yyyy'), 5000.00, null, 10);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7839, 'KING', '董事长', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 10);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7840, 'KK', '董事长', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 20);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7841, 'WK', '分析师', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 30);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7844, 'TURNER', '销售员', 7698, to_date('08-09-1981', 'dd-mm-yyyy'), 1500.00, 10.00, 30);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7876, 'ADAMS', '普通员工', 7788, to_date('23-05-1987', 'dd-mm-yyyy'), 1100.00, null, 20);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7900, 'JAMES', '普通员工', 7698, to_date('03-12-2017', 'dd-mm-yyyy'), 950.00, null, 30);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7902, 'FORD', '分析师', 7566, to_date('03-12-2009', 'dd-mm-yyyy'), 3000.00, null, 20);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (7934, 'MILLER', '普通员工', 7782, to_date('23-01-2000', 'dd-mm-yyyy'), 1300.00, null, 10);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (8888, '_LISI', '普通员工', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (8899, '刘%老师', '普通员工', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (8877, 'LISI', '普通员工', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 20);
-
-insert into SCOTT.EMP (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
-values (8890, 'Zhang', '普通员工', 7780, to_date('02-04-2006', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
-
-insert into SCOTT.SALGRADE (GRADE, LOSAL, HISAL)
-values (1, 700, 1200);
-
-insert into SCOTT.SALGRADE (GRADE, LOSAL, HISAL)
-values (2, 1201, 1400);
-
-insert into SCOTT.SALGRADE (GRADE, LOSAL, HISAL)
-values (3, 1401, 2000);
-
-insert into SCOTT.SALGRADE (GRADE, LOSAL, HISAL)
-values (4, 2001, 3000);
-
-insert into SCOTT.SALGRADE (GRADE, LOSAL, HISAL)
-values (5, 3001, 9999);
-
-commit;
-
---创建表
-create table SCOTT.STUDENT
+create table scott.emp
 (
-    ID   NUMBER,
-    NAME VARCHAR2(50)
+    empno    number(4) primary key,
+    ename    varchar2(50),
+    job      varchar2(20),
+    mgr      number(4),
+    hiredate date,
+    sal      number(7, 2),
+    comm     number(7, 2),
+    deptno   number(2)
+);
+alter table scott.emp
+    add constraint fk_deptno foreign key (deptno)
+        references scott.dept (deptno);
+
+create table scott.salgrade
+(
+    grade number,
+    losal number,
+    hisal number
 );
 
-create table SCOTT.ACHIEVEMENT
+create table scott.student
 (
-    ID   NUMBER,
-    MARK NUMBER
+    id   number,
+    name varchar2(50)
 );
 
-create table SCOTT.USERORGZ
+create table scott.achievement
 (
-    ID          NUMBER,
-    USERNAME    VARCHAR2(50),
-    MOBILEPHONE NUMBER
+    id   number,
+    mark number
 );
 
---插入数据
-insert into SCOTT.STUDENT (ID, NAME)
-values (1, 'Jack');
-
-insert into SCOTT.STUDENT (ID, NAME)
-values (2, 'Marry');
-
-insert into SCOTT.STUDENT (ID, NAME)
-values (3, 'Rose');
-
-insert into SCOTT.STUDENT (ID, NAME)
-values (4, 'Bob');
-
-insert into SCOTT.STUDENT (ID, NAME)
-values (5, 'John');
-
-insert into SCOTT.STUDENT (ID, NAME)
-values (6, 'Betty');
-
-insert into SCOTT.STUDENT (ID, NAME)
-values (7, 'Robert');
-
-insert into SCOTT.STUDENT (ID, NAME)
-values (8, 'Lisi');
-
-insert into SCOTT.ACHIEVEMENT (ID, MARK)
-values (1, 90);
-
-insert into SCOTT.ACHIEVEMENT (ID, MARK)
-values (2, 96);
-
-insert into SCOTT.ACHIEVEMENT (ID, MARK)
-values (3, 88);
-
-insert into SCOTT.ACHIEVEMENT (ID, MARK)
-values (4, 88);
-
-insert into SCOTT.ACHIEVEMENT (ID, MARK)
-values (5, 81);
-
-insert into SCOTT.ACHIEVEMENT (ID, MARK)
-values (6, 81);
-
-insert into SCOTT.ACHIEVEMENT (ID, MARK)
-values (8, 88);
-
-insert into SCOTT.USERORGZ (ID, USERNAME, MOBILEPHONE)
-values (1, '张三', 13115152323);
-
-insert into SCOTT.USERORGZ (ID, USERNAME, MOBILEPHONE)
-values (2, '李四', 13115152324);
-
-insert into SCOTT.USERORGZ (ID, USERNAME, MOBILEPHONE)
-values (3, '王五', 13115152325);
-
-insert into SCOTT.USERORGZ (ID, USERNAME, MOBILEPHONE)
-values (4, '小球', 13115152323);
-
-insert into SCOTT.USERORGZ (ID, USERNAME, MOBILEPHONE)
-values (5, '李小龙', 13115152324);
-
-insert into SCOTT.USERORGZ (ID, USERNAME, MOBILEPHONE)
-values (6, '王思', 13115152325);
-
-insert into SCOTT.USERORGZ (ID, USERNAME, MOBILEPHONE)
-values (7, '小王', 13115152326);
-
-insert into SCOTT.USERORGZ (ID, USERNAME, MOBILEPHONE)
-values (8, '小四', 13115152323);
-
-insert into SCOTT.USERORGZ (ID, USERNAME, MOBILEPHONE)
-values (9, '小三', 13115152327);
-
-insert into SCOTT.USERORGZ (ID, USERNAME, MOBILEPHONE)
-values (10, '小二', 13115152328);
-
-create table SCOTT.STOCK
+create table scott.userorgz
 (
-    MC VARCHAR2(50),
-    ZL NUMBER
+    id          number,
+    username    varchar2(50),
+    mobilephone number
 );
 
-create table SCOTT.SALES
+create table scott.stock
 (
-    MC VARCHAR2(50),
-    SL NUMBER
+    mc varchar2(50),
+    zl number
 );
 
-insert into SCOTT.STOCK (MC, ZL)
-values ('A', 100);
+create table scott.sales
+(
+    mc varchar2(50),
+    sl number
+);
 
-insert into SCOTT.STOCK (MC, ZL)
-values ('B', 150);
-
-insert into SCOTT.SALES (MC, SL)
-values ('A', 10);
-
-insert into SCOTT.SALES (MC, SL)
-values ('A', 20);
-
-insert into SCOTT.SALES (MC, SL)
-values ('B', 10);
-
-insert into SCOTT.SALES (MC, SL)
-values ('B', 20);
-
-insert into SCOTT.SALES (MC, SL)
-values ('B', 30);
-
-create table A (
+create table scott.a
+(
     月份 char(12),
     部门 char(2),
     业绩 number
 );
 
-insert into A 
-values('一月份', 'A', 10);
+-- 插入数据
+insert into scott.dept (deptno, dname, loc)
+values (10, '会计', '纽约');
 
-insert into A 
-values('一月份', 'B', 10);
+insert into scott.dept (deptno, dname, loc)
+values (20, '研发部', '达拉斯');
 
-insert into A 
-values('一月份', 'C', 5);
+insert into scott.dept (deptno, dname, loc)
+values (30, '销售部', '芝加哥');
 
-insert into A 
-values('二月份', 'A', 8);
+insert into scott.dept (deptno, dname, loc)
+values (40, '运营部', '波士顿');
 
-insert into A 
-values('二月份', 'B', 9);
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (8889, '王五', '普通员工', 7782, to_date('31-08-2017', 'dd-mm-yyyy'), 1230.00, null, null);
 
-insert into A 
-values('三月份', 'A', 8);
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7369, 'SM中文', '普通员工', 7902, to_date('17-12-1980', 'dd-mm-yyyy'), 800.00, null, 20);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7499, 'ALLEN', '销售员', 7698, to_date('20-02-1981', 'dd-mm-yyyy'), 1602.00, 301.00, 30);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7521, 'WARD', '销售员', 7698, to_date('22-02-1981', 'dd-mm-yyyy'), 1250.00, 500.00, 30);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7566, 'JONES', '经理', 7839, to_date('02-04-1981', 'dd-mm-yyyy'), 2975.00, null, 20);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7654, 'MARTIN', '销售员', 7698, to_date('28-09-1981', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7698, 'BLAKE', '经理', 7839, to_date('09-06-1981', 'dd-mm-yyyy'), 2850.00, null, 30);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7782, 'CLARK', '经理', 7839, to_date('09-06-1981', 'dd-mm-yyyy'), 2450.00, 1500.00, 10);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7788, 'SCOTT', '分析师', 7839, to_date('19-04-1987', 'dd-mm-yyyy'), 3000.00, null, 20);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7789, 'SCOTT', '分析师', 7566, to_date('20-04-1988', 'dd-mm-yyyy'), 5000.00, null, 10);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7839, 'KING', '董事长', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 10);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7840, 'KK', '董事长', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 20);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7841, 'WK', '分析师', null, to_date('17-11-1981', 'dd-mm-yyyy'), 5000.00, null, 30);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7844, 'TURNER', '销售员', 7698, to_date('08-09-1981', 'dd-mm-yyyy'), 1500.00, 10.00, 30);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7876, 'ADAMS', '普通员工', 7788, to_date('23-05-1987', 'dd-mm-yyyy'), 1100.00, null, 20);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7900, 'JAMES', '普通员工', 7698, to_date('03-12-2017', 'dd-mm-yyyy'), 950.00, null, 30);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7902, 'FORD', '分析师', 7566, to_date('03-12-2009', 'dd-mm-yyyy'), 3000.00, null, 20);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (7934, 'MILLER', '普通员工', 7782, to_date('23-01-2000', 'dd-mm-yyyy'), 1300.00, null, 10);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (8888, '_LISI', '普通员工', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (8899, '刘%老师', '普通员工', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (8877, 'LISI', '普通员工', 7782, to_date('02-04-2015', 'dd-mm-yyyy'), 1250.00, 1400.00, 20);
+
+insert into scott.emp (empno, ename, job, mgr, hiredate, sal, comm, deptno)
+values (8890, 'Zhang', '普通员工', 7780, to_date('02-04-2006', 'dd-mm-yyyy'), 1250.00, 1400.00, 30);
+
+insert into scott.salgrade (grade, losal, hisal)
+values (1, 700, 1200);
+
+insert into scott.salgrade (grade, losal, hisal)
+values (2, 1201, 1400);
+
+insert into scott.salgrade (grade, losal, hisal)
+values (3, 1401, 2000);
+
+insert into scott.salgrade (grade, losal, hisal)
+values (4, 2001, 3000);
+
+insert into scott.salgrade (grade, losal, hisal)
+values (5, 3001, 9999);
+
+insert into scott.student (id, name)
+values (1, 'Jack');
+
+insert into scott.student (id, name)
+values (2, 'Marry');
+
+insert into scott.student (id, name)
+values (3, 'Rose');
+
+insert into scott.student (id, name)
+values (4, 'Bob');
+
+insert into scott.student (id, name)
+values (5, 'John');
+
+insert into scott.student (id, name)
+values (6, 'Betty');
+
+insert into scott.student (id, name)
+values (7, 'Robert');
+
+insert into scott.student (id, name)
+values (8, 'Lisi');
+
+insert into scott.achievement (id, mark)
+values (1, 90);
+
+insert into scott.achievement (id, mark)
+values (2, 96);
+
+insert into scott.achievement (id, mark)
+values (3, 88);
+
+insert into scott.achievement (id, mark)
+values (4, 88);
+
+insert into scott.achievement (id, mark)
+values (5, 81);
+
+insert into scott.achievement (id, mark)
+values (6, 81);
+
+insert into scott.achievement (id, mark)
+values (8, 88);
+
+insert into scott.userorgz (id, username, mobilephone)
+values (1, '张三', 13115152323);
+
+insert into scott.userorgz (id, username, mobilephone)
+values (2, '李四', 13115152324);
+
+insert into scott.userorgz (id, username, mobilephone)
+values (3, '王五', 13115152325);
+
+insert into scott.userorgz (id, username, mobilephone)
+values (4, '小球', 13115152323);
+
+insert into scott.userorgz (id, username, mobilephone)
+values (5, '李小龙', 13115152324);
+
+insert into scott.userorgz (id, username, mobilephone)
+values (6, '王思', 13115152325);
+
+insert into scott.userorgz (id, username, mobilephone)
+values (7, '小王', 13115152326);
+
+insert into scott.userorgz (id, username, mobilephone)
+values (8, '小四', 13115152323);
+
+insert into scott.userorgz (id, username, mobilephone)
+values (9, '小三', 13115152327);
+
+insert into scott.userorgz (id, username, mobilephone)
+values (10, '小二', 13115152328);
+
+
+insert into scott.stock (mc, zl)
+values ('A', 100);
+
+insert into scott.stock (mc, zl)
+values ('B', 150);
+
+insert into scott.sales (mc, sl)
+values ('A', 10);
+
+insert into scott.sales (mc, sl)
+values ('A', 20);
+
+insert into scott.sales (mc, sl)
+values ('B', 10);
+
+insert into scott.sales (mc, sl)
+values ('B', 20);
+
+insert into scott.sales (mc, sl)
+values ('B', 30);
+
+insert into scott.a
+values ('一月份', 'A', 10);
+
+insert into scott.a
+values ('一月份', 'B', 10);
+
+insert into scott.a
+values ('一月份', 'C', 5);
+
+insert into scott.a
+values ('二月份', 'A', 8);
+
+insert into scott.a
+values ('二月份', 'B', 9);
+
+insert into scott.a
+values ('三月份', 'A', 8);
 
 commit;
 
-select * from SCOTT.SALGRADE;
+select *
+from scott.a;
 
-select * from SCOTT.USERORGZ;
+select *
+from scott.achievement;
 
-select * from SCOTT.STOCK;
+select *
+from scott.bonus;
 
-select * from SCOTT.SALES;
+select *
+from scott.student;
 
-select * from SCOTT.DEPT;
+select *
+from scott.salgrade;
 
-select * from SCOTT.EMP;
+select *
+from scott.userorgz;
+
+select *
+from scott.stock;
+
+select *
+from scott.sales;
+
+select *
+from scott.dept;
+
+select *
+from scott.emp;
